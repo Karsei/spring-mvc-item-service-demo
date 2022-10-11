dbcd738a2e4fd225402f3e63d0507ab8e211d3a1
Karsei <karseidev@gmail.com>
2022-10-11 23:15:14 +0900

    initial commit

---
+++ src/main/java/kr/pe/karsei/itemservice/web/item/BasicItemController.java
@@ -0,0 +1,25 @@
+package kr.pe.karsei.itemservice.web.item;
+
+import kr.pe.karsei.itemservice.domain.item.Item;
+import kr.pe.karsei.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping
+@RequiredArgsConstructor
+public class BasicItemController {
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+}
