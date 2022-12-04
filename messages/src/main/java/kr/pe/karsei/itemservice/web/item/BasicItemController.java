22e63bceca117ca2af09f139591fb50904b3b01c
Karsei <karseidev@gmail.com>
2022-12-04 14:14:11 +0900

    del: remove unnecessary codes

--- messages/src/main/java/kr/pe/karsei/itemservice/web/item/BasicItemController.java
+++ messages/src/main/java/kr/pe/karsei/itemservice/web/item/BasicItemController.java
@@ -45,53 +45,6 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
-//    @PostMapping("/add")
-    public String addItemV1(@RequestParam String itemName,
-                            @RequestParam int price,
-                            @RequestParam Integer quantity,
-                            Model model) {
-        Item item = new Item();
-        item.setItemName(itemName);
-        item.setPrice(price);
-        item.setQuantity(quantity);
-
-        itemRepository.save(item);
-
-        model.addAttribute("item", item);
-
-        return "basic/item";
-    }
-
-//    @PostMapping("/add")
-    public String addItemV2(@ModelAttribute("item") Item item,
-                            Model model) {
-        itemRepository.save(item);
-        //model.addAttribute("item", item); // 자동 추가, 생략 가능
-
-        return "basic/item";
-    }
-
-//    @PostMapping("/add")
-    public String addItemV3(@ModelAttribute Item item) { // Item -> 첫 글자를 소문자로 변경 -> item 으로 해서 model 에 담김) {
-        itemRepository.save(item);
-
-        return "basic/item";
-    }
-
-//    @PostMapping("/add")
-    public String addItemV4(Item item) {
-        itemRepository.save(item);
-
-        return "basic/item";
-    }
-
-//    @PostMapping("/add")
-    public String addItemV5(Item item) {
-        itemRepository.save(item);
-
-        return "redirect:/basic/items/" + item.getId();
-    }
-
     @PostMapping("/add")
     public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
         log.info("item.itemType={}", item.getItemType());
